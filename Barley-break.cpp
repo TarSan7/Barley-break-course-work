3e8044ee0dccc2ce2ef580964fe9f459165cae7e
Oleksandra Taratonova <kvbrd110820162@gmail.com>
2021-06-16 11:42:00 +0300

    Update Barley-break.cpp

--- Barley-break.cpp
+++ Barley-break.cpp
@@ -3,7 +3,6 @@
  Title: Barley-break
  Group: TV-91
  Student: Taratonova. O. S.
- Description: Перевірка на існування рішення гри П'ятнадцять
  ------------------------------------------------------------------</Header>-*/
 #include <iostream>
 #include <ctime>
@@ -13,15 +12,15 @@
 #include <fstream>
 using namespace std;
 
-//функція-генератор випадкових чисел
+// function-generator of random numbers
 void generate(int &trying, int &size, bool *checkit){ 
 	trying = rand() % (size * size); 	
-//перевірка на існування сгенерованого элемента;
+	// revision of the generated element;
 	if(!checkit[trying]){					
-//якщо зустрічаеться в перший раз, робимо відмітку в масиві
+		// itвЂ™s the first time to see it in the array
 		checkit[trying] = true;
 	} else{
-//якщо элемент вже зустрічався, ще раз генеруєм
+		// if the element is already being played, again it is generated
 		generate(trying, size, checkit);	
 	}
 }
@@ -32,20 +31,20 @@ cout << "Do you want to examine a random matrix or matrix from a file?(enter 'r'
 	cin >> ch;
 	if(ch != 'r' && ch != 'f'){
 		cout << "Incorrect input! Try again, please!\n";
-// при вводі некоректних даних повторно запускаемо функцію для ввода
+		// when entering incorrect data, re-run the function to enter
 		enter(size, ch);	
 
 	} else {
 		cout << "Enter size of field, please (the size can be 3, 4 or 100): ";
 		cin >> size; 
-//розмір поля може бути тільки 3, 4 або 100
+		// field size can only be 3, 4 or 100
 		if(size == 3 || size == 4 || size == 100){
-			system("cls");	//очистка экрана після введення правильных даних
+			system("cls");	// clear the screen after entering the correct data
 			cout << "Thank you for your choise!\n";
 		} else {
 			cout << "Incorrect input! Try again, please!\n";
 			enter(size, ch);
-//при вводі некоректних даних повторно запускаемо функцію для ввода
+			// when entering incorrect data, re-run the function to enter
 		}
 	}
 }
@@ -57,21 +56,21 @@ int main(){
 	while(1){
 		cout << "Hello, user!\n";
 		char ch;
-		int size;				//розмір поля пятнашек
-		enter(size, ch);			//функція для ввода розміру поля
-		bool checkit[size * size] = {0};	
-//масив для перевірки на існування елемента
-		vector <int> field;		//вектор для зберігання пятнашек
+		int size;				// size of the field of tags
+		enter(size, ch);			// function for entering the size of the field
+		bool checkit[size * size] = {0};
+		// array for converting to an element
+		vector <int> field;		
 		int temp[size];
-//задаємо масив, який знадобиться при заповненні вектора для економії місця
+		// specify the array that will be needed when filling the vector to save space
 		srand(time(NULL));
 		int trying = 0, ind = 0;
-//змінна для зберігання сгенерованого елемента та індекс для заповнення масива //temp[]
+		// variable to store the generated element and index to populate the array // temp []
 		if(size == 100) cout << "Field size is too large for printing.\n";
 		else cout << "Your field:\n";
 
 		for(int i = 0; i < size * size; i++){
-//функція заповнення масива числами
+			// function to fill the array with numbers
 			if(ch == 'r') generate(trying, size, checkit);
 			else{
 				if(!fin.eof()) fin >> trying;
@@ -84,20 +83,18 @@ int main(){
 					return 0;
 				}
 			}
-			if(i / size % 2 == 0) { 		//заповнення в прямому порядку
+			if(i / size % 2 == 0) { 		// fill in direct order
 				if(trying != 0) field.push_back(trying); 
-//додавання елемента в вектор
-				if(size != 100) cout << setw(4) << trying;
-//розмір дуже великий для виводу на екран
+				// add an element to the vector
+				if(size != 100) cout << setw(4) << trying; // the size is too large for display
 			}
-			else{ 			//заповнення строки поля в зворотному порядку
+			else{ 			// fill in the field in the reverse order
 				temp[ind] = trying;
 				if(size != 100) cout << setw(4) << temp[ind];
 				ind++;
-				if((i + 1) % size == 0){	//перенесення строки поля в вектор
+				if((i + 1) % size == 0){	// transfer the field string to the vector
 				    for(int j = size - 1; j >= 0; j--){
 				        if(temp[j] != 0) field.push_back(temp[j]);
-//не вносим порожню клітку
 				    }
 				  ind = 0;
 			     }
@@ -105,14 +102,13 @@ int main(){
 		    if((i + 1) % size == 0 && size != 100) cout << '\n';
 		}
 
-		int number = 1;			//елемент для пошуку в векторі
+		int number = 1;			// element to search in vector
 		while(1){
 		    for(int i = number - 1; i < field.size(); i++){
 		       if(field[i] == number && i != number - 1){ 
-//перенос елемента на парну кількість елементів
-			   field.insert(field.begin() + number - 1 + (i - number + 1) % 2, field[i]);
-//видалення перенесеного елементу
-					field.erase(field.begin() + i + 1);									//якщо перенесли не на своє місце, а на поруч стояче
+			       // transfer an element to an even number of elements
+			   field.insert(field.begin() + number - 1 + (i - number + 1) % 2, field[i]);// delete the transferred item
+					field.erase(field.begin() + i + 1);				
 					if((i - number + 1) % 2 == 1){		
 					   field.insert(field.begin() + number + 2, field[number - 1]);
 					   field.erase(field.begin() + number - 1);
@@ -121,17 +117,15 @@ int main(){
 				}
 			}
 			number++;
-//умова на вихід з циклу(елемент для пошуку - кінець вектора)
 			if(number == field.size() - 1) break;
 		}
 
 		cout << endl;	
 
-//чи стоять останні 2 елемента правильно чи ні
 		if(field[size * size - 3] - field[size * size - 2] == 1) cout << "Solution of this location exists!\n";
 		else cout << "No exist solution of this location.....\n";
 
-		cout << "Enter 'e' for exit or 'r' for repeat.\n";       //можливість перезапустити
+		cout << "Enter 'e' for exit or 'r' for repeat.\n";      
 		cin >> ch;
 		if(ch == 'e'){
 			cout << "Goodbye!"; 
